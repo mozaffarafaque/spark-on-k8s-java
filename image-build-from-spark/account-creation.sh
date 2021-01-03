3ef3890884539e0ccd575e305dd19be5685bc9b2
Mozaffar Afaque <afaque.mozaffar@gmail.com>
2021-01-03 13:07:52 +0530

    Test app

--- image-build-from-spark/account-creation.sh
+++ image-build-from-spark/account-creation.sh
@@ -4,3 +4,6 @@ kubectl create namespace spark
 kubectl create serviceaccount spark -n spark
 
 kubectl create clusterrolebinding spark-role --clusterrole=edit --serviceaccount=spark:spark --namespace=spark
+
+# After applying kubectl apply command
+kubectl port-forward -n spark deployment.apps/my-notebook-deployment 8888:8888
