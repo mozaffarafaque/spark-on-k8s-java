eca540f996e8ac760c84df48574611ec4482daac
Mozaffar Afaque <afaque.mozaffar@gmail.com>
2021-01-06 00:55:21 +0530

    Test app

--- image-build-from-spark/spark-operations.sh
+++ image-build-from-spark/spark-operations.sh
@@ -10,12 +10,15 @@ kubectl create clusterrolebinding spark-role --clusterrole=edit  --serviceaccoun
 
 
 bin/spark-submit \
---master k8s:// https://172.31.80.10:6443 \
+--master k8s://https://172.31.80.10:6443 \
 --deploy-mode cluster \
 --name spark-pi \
 --class org.apache.spark.examples.SparkPi \
 --conf spark.executor.instances=2 \
---conf spark.kubernetes.container.image=mozafaq/spark-k8s:3.1 \
+--conf spark.kubernetes.container.image=mozafaq/spark-on-k8s:3.1 \
 --conf spark.kubernetes.container.image.pullPolicy=Never \
 --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
 local:///opt/spark/examples/jars/spark-examples_2.12-3.1.1-SNAPSHOT.jar 100000
+
+
+mozafaq/spark-on-k8s:3.1
